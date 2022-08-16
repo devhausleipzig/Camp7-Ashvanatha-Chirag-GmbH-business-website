6aba24a62a626eac2ec0f3fbb67eaf731e47c1cf
Chirag Singal <chirag.prakriya@gmail.com>
2022-08-16 13:46:55 +0200

    logo hover effect and nav bar basic v1

---
+++ script.js
@@ -0,0 +1,12 @@
+//navbar slide from https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-150px";
+  }
+  prevScrollpos = currentScrollPos;
+};
