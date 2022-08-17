bd81100b1c1b784ba94d0d6d7adacbb32e274b23
ashvntha <ashvntha@ashvnthas-MacBook-Pro.local>
2022-08-17 16:02:14 +0200

    style faq accordion and add content

--- script.js
+++ script.js
@@ -25,3 +25,31 @@ accArrowWhen.addEventListener("click", function (event) {
 	accFaqBody.classList.toggle("unhidden");
 	accArrowWhen.classList.toggle("upside-down");
 });
+
+const accArrowWhere = document.querySelector("#acc-arrow-where");
+const accFaqBody2 = document.querySelector("#acc-faq-body2");
+accArrowWhere.addEventListener("click", function (event) {
+	accFaqBody2.classList.toggle("unhidden");
+	accArrowWhere.classList.toggle("upside-down");
+});
+
+const accArrowWill = document.querySelector("#acc-arrow-will");
+const accFaqBody3 = document.querySelector("#acc-faq-body3");
+accArrowWill.addEventListener("click", function (event) {
+	accFaqBody3.classList.toggle("unhidden");
+	accArrowWill.classList.toggle("upside-down");
+});
+
+const accArrowFree = document.querySelector("#acc-arrow-free");
+const accFaqBody4 = document.querySelector("#acc-faq-body4");
+accArrowFree.addEventListener("click", function (event) {
+	accFaqBody4.classList.toggle("unhidden");
+	accArrowFree.classList.toggle("upside-down");
+});
+
+const accArrowWait = document.querySelector("#acc-arrow-wait");
+const accFaqBody5 = document.querySelector("#acc-faq-body5");
+accArrowWait.addEventListener("click", function (event) {
+	accFaqBody5.classList.toggle("unhidden");
+	accArrowWait.classList.toggle("upside-down");
+});
