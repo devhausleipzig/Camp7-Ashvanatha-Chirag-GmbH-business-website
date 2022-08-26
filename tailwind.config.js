45d470adca719dc53c9b7daf6caed6419540591f
Chirag Singal <chirag.prakriya@gmail.com>
2022-08-26 10:17:44 +0200

    added colors in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,ts,css}"],
 	theme: {
+		colors: {
+			lightBeige: "#fff7f0",
+			blue: "#0568fd",
+			purple: "#6c64ce",
+			lightPurple: "#b8b2f2",
+			turquoise: "#38b3d1",
+			pink: "#f290e8",
+			lightPink: "#fac8f5",
+			black: "#000000",
+			white: "#ffffff",
+		},
 		extend: {},
 	},
 	plugins: [],
