3a439bdb9ca1449795e13a5616f8646329d83c28
ashvntha <ashvntha@ashvnthas-MacBook-Pro.local>
2022-08-28 19:50:53 +0200

    add marquee styling ticker

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,22 @@ module.exports = {
 			black: "#000000",
 			white: "#ffffff",
 		},
-		extend: {},
+		extend: {
+			animation: {
+				marquee: "marquee 30s linear infinite",
+				marquee2: "marquee2 30s linear infinite",
+			},
+			keyframes: {
+				marquee: {
+					"0%": { transform: "translateX(0%)" },
+					"100%": { transform: "translateX(-100%)" },
+				},
+				marquee2: {
+					"0%": { transform: "translateX(100%)" },
+					"100%": { transform: "translateX(0%)" },
+				},
+			},
+		},
+		plugins: [],
 	},
-	plugins: [],
 };
